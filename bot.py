efa5a81a4e1ef86f53968aa5c0cf3d51088eb39f
Nathaniel Duya <nathanduya@gmail.com>
2020-12-30 19:48:19 -0800

    Added Firefox functionality, Added ability to change card month/year, removed unnecessary files

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ def order(k):
     driver.get(k['product_url'])
 
     driver.find_element_by_xpath('//*[@id="add-remove-buttons"]/input').click()
-    time.sleep(.5)
+    time.sleep(.1)
     driver.find_element_by_xpath('//*[@id="cart"]/a[2]').click()
 
     # personal info
@@ -17,20 +17,18 @@ def order(k):
     driver.find_element_by_xpath('//*[@id="order_billing_zip"]').send_keys(k["zip"])
     driver.find_element_by_xpath('//*[@id="order_billing_city"]').send_keys(k["city"])
 
-    # payment
+    # card info/payment
     driver.find_element_by_xpath('//*[@id="rnsnckrn"]').send_keys(k["card"])
     driver.find_element_by_xpath('//*[@id="orcer"]').send_keys(k["cvv"])
-    driver.find_element_by_xpath('//*[@id="credit_card_month"]/option[4]').click()
-    driver.find_element_by_xpath('//*[@id="credit_card_year"]/option[3]').click()
-
-    # ADD MORE FUNCTIONALITY BY ADDING MONTH TO CONFIG FILE
-    # driver.find_element_by_xpath('//*[@id="credit_card_month"]/option[{month}]').format(k["card_month"]).click
-    # YEAR NEEDS TO BE CHANGED EVERY YEAR BY 1-10 1 BEING CURRENT YEAR
+    driver.find_element_by_xpath('//*[@id="credit_card_month"]/option[{}]'.format(k["card_month"])).click()
+    driver.find_element_by_xpath('//*[@id="credit_card_year"]/option[{}]'.format(k["card_year"])).click()
 
     # terms and conditions & process payment
     driver.find_element_by_xpath('//*[@id="cart-cc"]/fieldset/p/label/div/ins').click()
     driver.find_element_by_xpath('//*[@id="pay"]/input').click()
 
 if __name__ == '__main__':
-    driver = webdriver.Chrome('./chromedriver')
+    #driver = webdriver.Chrome('./chromedriver')
+    driver = webdriver.Firefox(executable_path = './geckodriver')
+#if user selects chrome or firefox set driver = 
     order(keys)
--- config.py
+++ config.py
@@ -1,11 +1,18 @@
 keys = {
     "product_url": "https://www.supremenewyork.com/shop/accessories/bfgiovyw7",
+
+    #personal info
     "name": "Nathaniel Duya",
     "email": "nathanielduya@yahoo.com",
     "phone": "4155358679",
     "address": "70 West Cavour St",
-    "zip": "94014",
+    "zip": 94014,
     "city": "Daly City",
-    "card": "12324214",
-    "cvv": "213"
+
+    #card info
+    "card": 12324214,
+    "cvv": 213,
+    "card_month": 5,
+    "card_year": 5
+
 }
